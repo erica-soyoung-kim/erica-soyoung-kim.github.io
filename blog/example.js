0ac95d4dd2b6c2d997e311fd86dce5c5d37213cd
So Young Kim <erica.s.y.kim.94@gmail.com>
2024-03-18 12:25:14 +1300

    sprint 3 technical blog

---
+++ blog/example.js
@@ -0,0 +1,23 @@
+
+
+
+// Example of JavaScript control flow for working day
+
+// Step 1: You have a working day
+let workingDay = true;
+
+// Step 2: Check if you are working today
+if (workingDay) {
+    // Step 3: If you are working today, go to work
+    console.log("Go to work")
+
+} else {
+    // Step 4: If you are not working today, stay home and do some housework
+    console.log("stay home and do some housework");
+}
+
+
+
+For example, let's say you are going to work some days in a week. <br>
+          When you wake up, if it is your working day, then you go to work. <br>
+          Or else (if it is NOT your working day), then you do not go to work, stay at home and do some housework.
